2f98fcf1e9b0424acd03407b180dd8a787c8c55c
Alexandre NEDJARI <alex.nedjari@gmail.com>
2014-10-05 19:20:26 +0200

    Brewery management (add, delete)

--- client/app/brewery/brewery.controller.js
+++ client/app/brewery/brewery.controller.js
@@ -1,6 +1,35 @@
 'use strict';
 
 angular.module('mishkaBeerApp')
-  .controller('BreweryCtrl', function ($scope) {
-    $scope.message = 'Hello';
+.controller('BreweryCtrl', function ($scope, $http, socket) {
+  $scope.brewery;
+  $scope.brewerys = [];
+
+
+  $scope.initBrewery = function() {
+      $scope.brewery = {name:''};
+  }
+
+  $http.get('/api/brewery').success(function(brewerys) {
+    $scope.brewerys = brewerys;
+    socket.syncUpdates('brewery', $scope.brewerys);
   });
+
+  $scope.deleteBrewery = function(brewery) {
+    $http.delete('/api/brewery/' + brewery._id);
+  };
+
+  $scope.$on('$destroy', function () {
+    socket.unsyncUpdates('brewery');
+  });
+
+  $scope.addBrewery = function() {
+    if($scope.brewery.name === '') {
+      return;
+    }
+    $http.post('/api/brewery',$scope.brewery);
+    $scope.initBrewery();
+  };
+  $scope.initBrewery();
+});
+
