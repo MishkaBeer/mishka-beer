bdcbfdfe626a7728ad515853735a481ae0252869
MatthieuG <gaudetmatthieu@gmail.com>
2014-12-20 13:27:39 +0100

    Improve hops management.

--- client/app/hops/hops.controller.js
+++ client/app/hops/hops.controller.js
@@ -1,87 +1,113 @@
 'use strict';
 
 angular.module('mishkaBeerApp')
-  .controller('HopsCtrl', function ($scope, $http, socket, $translate) {
-    $scope.hops = [];
-    $scope.newHop = {
-        "$edit" : true
-    };
-    $scope.newHopClass = "";
-    $scope.currentHopInList = null;
-    $scope.errorGetList = false;
+    .controller('HopsCtrl', function ($scope, $http, socket, $translate) {
+        $scope.editInfos = [];
+        $scope.hops = [];
 
-    $scope.editHop = function($hop) {
-        if ($scope.currentHopInList != null) {
-            $scope.currentHopInList.$edit = false;
-        }
-        if ($scope.currentHopInList != $hop || $hop.$details) {
-            $scope.currentHopInList = $hop;
-            $hop.$edit = true;
-            $hop.$details = false;
-        } else {
-            $hop.$edit = false;
-            $hop.$details = false;
-            $scope.currentHopInList = null;
-        }
-    }
-
-    $scope.showHop = function($hop) {
-
-        if ($scope.currentHopInList != null) {
-            $scope.currentHopInList.$details = false;
-        }
-        if ($scope.currentHopInList != $hop || $hop.$edit) {
-            $scope.currentHopInList = $hop;
-            $hop.$edit = false;
-            $hop.$details = true;
-        } else {
-            $hop.$edit = false;
-            $hop.$details = false;
-            $scope.currentHopInList = null;
-        }
-    }
-
-    $scope.changeShowNewHop = function() {
-        if ( $scope.newHopClass === "") {
-            $scope.newHopClass = "in";
-        } else {
-            $scope.newHopClass = "";
-        }
-    }
-
-    $http.get('/api/hops')
-        .success(function(hops) {
-          $scope.hops = hops;
-
-          socket.syncUpdates('hop', $scope.hops, function(event, item, list, oldItem) {
-            if (oldItem != null && oldItem.$edit) {
-                item.$edit = true;
+        /**
+         * Return infos for an element.
+         *
+         * @param {Integer} id
+         */
+        $scope.getInfos = function (id) {
+            for (var i in $scope.editInfos) {
+                if ($scope.editInfos[i].id == id) {
+                    return $scope.editInfos[i];
+                }
             }
-            if (oldItem != null && oldItem.$alert) {
-                item.$alert = true;
-            }
-          }
-        );
-    }).error(function(hops) {
-            $scope.errorGetList = false;
+            return null;
         }
-    );
 
-    $scope.saveHop = function($hop) {
-        $scope.hops.push($hop);
-        if ($hop._id != null) {
-            return $http.put('/api/hops/' + $hop._id, $hop);
-        } else {
-            return $http.post('/api/hops', $hop);
+
+
+        /**
+         * Function to close all
+         */
+        $scope.closeAll = function () {
+            for (var i = 0; i < $scope.editInfos.length; i++) {
+                $scope.editInfos[i].$edit = false;
+                $scope.editInfos[i].$details = false;
+            }
+        }
+
+        $scope.edit = function ($element) {
+            $scope.closeAll();
+            var info = $scope.getInfos($element._id);
+            info.$edit = true;
+            info.$details = false;
+        }
+
+        $scope.show = function ($element) {
+            $scope.closeAll();
+            var info = $scope.getInfos($element._id);
+            info.$edit = false;
+            info.$details = true;
+        }
+
+
+        //
+        // Hops services.
+        //
+
+        $scope.changeShowNewHop = function () {
+            if ($scope.newHopClass === "") {
+                $scope.newHopClass = "in";
+            } else {
+                $scope.newHopClass = "";
+            }
+        }
+
+        $scope.newHopClass = "";
+
+        $scope.errorGetList = false;
+
+        $http.get('/api/hops')
+            .success(function (hops) {
+
+                $scope.hops = hops;
+                $scope.editInfos = [];
+                for (var i = 0; i < hops.length; i++) {
+                    $scope.editInfos.push({
+                        $edit: false,
+                        $details: false,
+                        id: hops[i]._id
+                    });
+                }
+
+                socket.syncUpdates('hop', $scope.hops, function (event, item, list, oldItem) {
+                    if (event === "deleted") {
+                        _.remove($scope.editInfos, {
+                            id: item._id
+                        });
+                    } else if (event === "created") {
+                            $scope.editInfos.push({
+                                $edit: false,
+                                $details: false,
+                                id: item._id
+                            });
+                        console.log("=>>> taille editInfos ap cre : " + $scope.editInfos.length);
+                    }
+                });
+                $scope.errorGetList = false;
+            }).error(function (hops) {
+                $scope.errorGetList = true;
+            });
+
+        $scope.saveHop = function ($hop) {
+            if ($hop._id != null) {
+                return $http.put('/api/hops/' + $hop._id, $hop);
+            } else {
+                return $http.post('/api/hops', $hop);
+            };
         };
-    };
 
-    $scope.deleteHop = function(hop) {
-      $http.delete('/api/hops/' + hop._id);
-    };
+        $scope.deleteHop = function ($hop) {
+            $http.delete('/api/hops/' + $hop._id);
+        };
 
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('hop');
+        $scope.$on('$destroy', function () {
+            socket.unsyncUpdates('hop');
+        });
+
     });
-
-  });
