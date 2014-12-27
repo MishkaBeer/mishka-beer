e151fde6ecfa655f6823cbeb0603411562f25289
MatthieuG <gaudetmatthieu@gmail.com>
2014-12-27 15:36:53 +0100

    JSLint check little changes

--- client/components/utils/msk-utilities.service.js
+++ client/components/utils/msk-utilities.service.js
@@ -1,86 +1,87 @@
 angular.module("mishkaBeerApp").factory("$mskUtilities",
     function ($http, socket) {
+        'use strict';
 
-        createListEditManagerFct = function ($apiUrl, $listName) {
-            return {
-                items: [],
-                editInfos: [],
-                apiUrl: $apiUrl,
-                listName: $listName,
-                initList: function () {
-                    listItems = this.items;
-                    name = this.listName;
-                    return $http.get(this.apiUrl).success(function ($listReceived) {
-                        this.editInfos = [];
-                        for (var i = 0; i < $listReceived.length; i++) {
-                            this.editInfos.push({
+        var ListEditManager = function ($apiUrl, $listName) {
+            var manager = {};
+            manager.items = [];
+            manager.editInfos = [];
+            manager.apiUrl = $apiUrl;
+            manager.listName = $listName;
+            manager.initList = function () {
+                return $http.get(this.apiUrl).success(function ($listReceived) {
+                    var i;
+                    for (i = 0; i < $listReceived.length; i = i + 1) {
+                        manager.editInfos.push({
+                            $edit: false,
+                            $details: false,
+                            id: $listReceived[i]._id
+                        });
+                        manager.items.push($listReceived[i]);
+                    }
+                    socket.syncUpdates(manager.listName, manager.items, function (event, item, list, oldItem) {
+                        if (event === "deleted") {
+                            _.remove(manager.editInfos, {
+                                id: item._id
+                            });
+                        } else if (event === "created") {
+                            manager.editInfos.push({
                                 $edit: false,
                                 $details: false,
-                                id: $listReceived[i]._id
+                                id: item._id
                             });
-                            this.listItems.push($listReceived[i]);
                         }
-                        socket.syncUpdates(this.name, this.listItems, function (event, item, list, oldItem) {
-                            if (event === "deleted") {
-                                _.remove(this.editInfos, {
-                                    id: item._id
-                                });
-                            } else if (event === "created") {
-                                this.editInfos.push({
-                                    $edit: false,
-                                    $details: false,
-                                    id: item._id
-                                });
-                            }
-                        });
-                    })
-                },
-                save: function ($item) {
-                    return $http.put(this.apiUrl + $item._id, $item);
-                },
-                saveNew: function ($item) {
-                    return $http.post(this.apiUrl, $item);
-                },
-                delete: function ($item) {
-                    return $http.delete(this.apiUrl + $item._id);
-                },
-                getInfos: function (id) {
-                    for (var i in editInfos) {
-                        if (editInfos[i].id == id) {
-                            return editInfos[i];
-                        }
+                    });
+                });
+            };
+            manager.save = function ($item) {
+                return $http.put(manager.apiUrl + $item._id, $item);
+            };
+            manager.saveNew = function ($item) {
+                return $http.post(manager.apiUrl, $item);
+            };
+            manager.delete = function ($item) {
+                return $http.delete(tmanager.apiUrl + $item._id);
+            };
+            manager.getInfos = function (id) {
+                var i;
+                for (i = 0; i < manager.editInfos.length; i = i + 1) {
+                    if (manager.editInfos[i].id === id) {
+                        return manager.editInfos[i];
                     }
-                    return null;
-                },
-                closeAll: function () {
-                    for (var i = 0; i < editInfos.length; i++) {
-                        editInfos[i].$edit = false;
-                        editInfos[i].$details = false;
-                    }
-                },
-                edit: function ($element) {
-                    var info = this.getInfos($element._id);
-                    var editNew = !info.$edit;
-                    this.closeAll();
-                    info.$edit = editNew;
-                    info.$details = false;
-                },
-                show: function ($element) {
-                    var info = this.getInfos($element._id);
-                    var detailsNew = !(info.$details || info.$edit);
-                    this.closeAll();
-                    info.$edit = false;
-                    info.$details = detailsNew;
-                },
-                unsyncUpdates: function () {
-                    socket.unsyncUpdates('malt');
                 }
-            }
+                return null;
+            };
+            manager.closeAll = function () {
+                var i;
+                for (i = 0; i < manager.editInfos.length; i = i + 1) {
+                    manager.editInfos[i].$edit = false;
+                    manager.editInfos[i].$details = false;
+                }
+            };
+            manager.edit = function ($element) {
+                var info = manager.getInfos($element._id),
+                    editNew = !info.$edit;
+                manager.closeAll();
+                info.$edit = editNew;
+                info.$details = false;
+            };
+            manager.show = function ($element) {
+                var info = manager.getInfos($element._id),
+                    detailsNew = !(info.$details || info.$edit);
+                manager.closeAll();
+                info.$edit = false;
+                info.$details = detailsNew;
+            };
+            manager.unsyncUpdates = function () {
+                socket.unsyncUpdates('malt');
+            };
+
+            return manager;
         };
 
         return {
             /** creation of a list manager */
-            createListEditManager: createListEditManagerFct
+            createListEditManager: ListEditManager
         };
-    }
-);
+    });
