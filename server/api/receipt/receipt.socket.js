53e56f4cde598e03f29ec507bb8ecb4b3eb12995
MatthieuG <gaudetmatthieu@gmail.com>
2014-12-29 21:08:24 +0100

    Generator example "thing" removed on server side

--- server/api/receipt/receipt.socket.js
+++ server/api/receipt/receipt.socket.js
@@ -4,21 +4,21 @@
 
 'use strict';
 
-var thing = require('./thing.model');
+var Receipt = require('./receipt.model');
 
 exports.register = function(socket) {
-  thing.schema.post('save', function (doc) {
+  Receipt.schema.post('save', function (doc) {
     onSave(socket, doc);
   });
-  thing.schema.post('remove', function (doc) {
+  Receipt.schema.post('remove', function (doc) {
     onRemove(socket, doc);
   });
 }
 
 function onSave(socket, doc, cb) {
-  socket.emit('thing:save', doc);
+  socket.emit('receipt:save', doc);
 }
 
 function onRemove(socket, doc, cb) {
-  socket.emit('thing:remove', doc);
+  socket.emit('receipt:remove', doc);
 }
--- server/api/receipt/receipt.spec.js
+++ server/api/receipt/receipt.spec.js
@@ -4,11 +4,11 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
-describe('GET /api/things', function() {
+describe('GET /api/receipts', function() {
 
   it('should respond with JSON array', function(done) {
     request(app)
-      .get('/api/things')
+      .get('/api/receipts')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
